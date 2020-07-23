2f06ed7b9e890eafc86d8987bab5a8b4fb9b6379
Daniel Caldera <dan@MacBook-Air-de-Daniel.local>
2020-07-23 13:26:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// Cors permite que un cliente se conecta a otro servidor para el intercambio de recursos
+
+const cors = require('cors')
+
+const url = `mongodb+srv://mhdbuser:admin123@cluster0-1epea.mongodb.net/store?retryWrites=true&w=majority`
+
+// conectar mongo
+mongoose.Promise = global.Promise
+mongoose.connect(url, {
+  useNewUrlParser: true,
+})
+
+// crear el servidor
+const app = express()
+
+// habilitar bodyparser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Habilitar cors
+app.use(cors())
+
+// Rutas de la app
+app.use('/', routes())
+
+// carpeta publica
+app.use(express.static('uploads'))
+
+// puerto
+app.listen(5000)
